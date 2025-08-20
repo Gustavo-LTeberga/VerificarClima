7cf1fcad152a4fc5280520a721b1b12d9e0c3fb8
Gaiozh <gustavogaiozh@gmail.com>
2025-08-20 19:50:26 -0300

    Finalização do Programa
    Fiz toda função de verificar temperatura, além de tratar erros como, não deixar digitar letras, ou se estiver vazio avisar e não executar o programa, e coloquei para mostrar o dia de hoje

--- VerificarClima/Form1.Designer.cs
+++ VerificarClima/Form1.Designer.cs
@@ -24,61 +24,63 @@
         /// </summary>
         private void InitializeComponent() {
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pibStatus = new System.Windows.Forms.PictureBox();
+            this.lblDiaHoje = new System.Windows.Forms.Label();
+            this.txbTemp = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pibStatus)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(156, 44);
+            this.lblTitulo.Location = new System.Drawing.Point(156, 50);
             this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(205, 20);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Verificador De Temperatura";
             // 
-            // textBox1
+            // pibStatus
             // 
-            this.textBox1.Location = new System.Drawing.Point(160, 85);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(201, 26);
-            this.textBox1.TabIndex = 1;
+            this.pibStatus.Location = new System.Drawing.Point(160, 129);
+            this.pibStatus.Name = "pibStatus";
+            this.pibStatus.Size = new System.Drawing.Size(201, 168);
+            this.pibStatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibStatus.TabIndex = 2;
+            this.pibStatus.TabStop = false;
             // 
-            // pictureBox1
+            // lblDiaHoje
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(160, 129);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(201, 168);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.lblDiaHoje.AutoSize = true;
+            this.lblDiaHoje.Location = new System.Drawing.Point(198, 320);
+            this.lblDiaHoje.Name = "lblDiaHoje";
+            this.lblDiaHoje.Size = new System.Drawing.Size(0, 20);
+            this.lblDiaHoje.TabIndex = 3;
             // 
-            // label1
+            // txbTemp
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 319);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.txbTemp.Location = new System.Drawing.Point(160, 86);
+            this.txbTemp.Name = "txbTemp";
+            this.txbTemp.Size = new System.Drawing.Size(201, 26);
+            this.txbTemp.TabIndex = 4;
+            this.txbTemp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbTemp_KeyDown);
+            this.txbTemp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbTemp_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 359);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(547, 359);
+            this.Controls.Add(this.txbTemp);
+            this.Controls.Add(this.lblDiaHoje);
+            this.Controls.Add(this.pibStatus);
             this.Controls.Add(this.lblTitulo);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Temperatura";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibStatus)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,9 +89,9 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pibStatus;
+        private System.Windows.Forms.Label lblDiaHoje;
+        private System.Windows.Forms.TextBox txbTemp;
     }
 }
 
--- VerificarClima/Form1.cs
+++ VerificarClima/Form1.cs
@@ -13,5 +13,41 @@ namespace VerificarClima {
         public Form1() {
             InitializeComponent();
         }
+
+        private void txbTemp_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+
+                if (txbTemp.Text == "") {
+                    MessageBox.Show("Erro a temperatura esta vazia!", "Erro!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else {
+
+                    double temperatura = double.Parse(txbTemp.Text);
+
+                    if (temperatura > 30) {
+                        pibStatus.Image = Properties.Resources.hot;
+                    }
+                    else if (temperatura >= 15 && temperatura <= 30) {
+                        pibStatus.Image = Properties.Resources.temperature;
+                    }
+                    else {
+                        pibStatus.Image = Properties.Resources.freeze;
+                    }
+
+                    lblDiaHoje.Text = $"Dia de hoje: {DateTime.Now.Day.ToString()}/{DateTime.Now.Month}"; 
+
+                }
+
+
+            }
+        }
+
+        private void txbTemp_KeyPress(object sender, KeyPressEventArgs e) {
+
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != '-' && e.KeyChar != (char)Keys.Back)  {
+                e.Handled = true;
+            }
+
+        }
     }
 }
